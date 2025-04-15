27ee3d314c70a97806bdf12cae602bbe18f6455b
Thanh Ha <thanh.ha@s3corp.com.vn>
2025-04-15 18:10:26 +0700

    fix docker file

--- vite.config.ts
+++ vite.config.ts
@@ -15,9 +15,11 @@ export default defineConfig({
     },
   },
   server: {
+    host: '0.0.0.0',
+    port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // Replace with your backend API URL
+        target: 'http://35.185.181.50', // Replace with your backend API URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
